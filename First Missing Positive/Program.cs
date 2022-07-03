321e86b14419961411f0abd25718fac2cb9f3dd5
jd_786 <jaydeep_shil@certree.com>
2022-07-03 20:15:32 +0530

    new code

--- First Missing Positive/Program.cs
+++ First Missing Positive/Program.cs
@@ -6,52 +6,57 @@ namespace First_Missing_Positive
   {
     static void Main(string[] args)
     {
-      Program p = new Program();
       int[] nums = new int[] { 0, 3, 1, 5, 2 }; // 1, 2, 0 // 3, 4, -1, 1 // 7, 8, 9, 11, 12 // 1, 2, 3, 4 , 5
-      Console.WriteLine(p.FirstMissingPositive(nums));
+      Solution s = new Solution();
+      Console.WriteLine(s.FirstMissingPositive(nums));
     }
 
-    public int FirstMissingPositive(int[] nums)
+    public class Solution
     {
-      bool containsOne = false;
-      int length = nums.Length;
-      // Step 1 -
-      // loop entire array and mark the cells as 1 wherever cell value is <= 0 or value > N, also check input contains 1 as cell value or not.
-      // if 1 value is not present in the given input, blindly we can return 1 as an output.
-      for (int i = 0; i < length; i++)
+      public int FirstMissingPositive(int[] nums)
       {
-        if(nums[i] == 1)
+        int length = nums.Length;
+        // Step 1 -
+        // loop entire array and mark the cells as 0 wherever cell value is < 0, negetive
+        // coz the smallest positive int value is 1 and negetive values are havong no meaning here
+        for (int i = 0; i < length; i++)
         {
-          containsOne = true;
+          if (nums[i] < 0)
+          {
+            nums[i] = 0;
+          }
         }
-        if (nums[i] <= 0 || nums[i] > length)
+
+        // Step 2 -
+        // loop entire array and for each abs value of cell mark it as present 
+        // how ? absVal - 1 would be the array index where we set the current cell value to * -1
+        // or if the cell value is 0 which is possible due to the negetive value we made it to negetive from step 1
+        // will set it with -1 * length + 1
+        for (int i = 0; i < length; i++)
         {
-          nums[i] = 1;
+          // take the abs value, as there can be a value still negetive as we are modifying the input array cell to negetive to mark a value exist
+          int absVal = Math.Abs(nums[i]);
+          if (absVal >= 1 && absVal <= length)
+          {
+            int index = absVal - 1;
+            if (nums[index] > 0)
+              nums[index] *= -1;
+            else if (nums[index] == 0)
+              nums[index] = -1 * (length + 1);
+          }
         }
+        // Step 3 -
+        for (int i = 1; i <= length; i++)
+        {
+          int index = i - 1;
+          // in step 3 if we find any positive value in cell which tell that the missing value is found
+          if (nums[index] >= 0)
+          {
+            return i;
+          }
+        }
+        return length + 1;
       }
-
-      // if 1 is not present in input array.
-      if (!containsOne)
-      {
-        return 1;
-      }
-
-      // Step 2 -
-      // loop entire array and for each cell value -1 position, update to negetive to mark that value is present in range of 1-N
-      for (int i = 0; i < length; i++)
-      {
-        int val = Math.Abs(nums[i]);
-        if(nums[val - 1] > 0)
-          nums[val - 1] *= -1;
-      }
-
-      // Step 3 -
-      // loop entire array, any value as positive , that position + 1 is the missing element, if not found then N+1 is the missing element(eg- (1,2,3,4,5)=>O/p-6)
-      for (int i = 0; i < length; i++)
-      {
-        if (nums[i] > 0) return i + 1;
-      }
-      return length + 1;
     }
   }
 }
